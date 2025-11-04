9548590dfe29145899730d6a2114c4696c98f4ed
Advaith <advaithvelavan39@gmail.com>
2025-11-04 13:15:27 +0000

    implement calculations of potentials phi and psi

--- sinkhorn.py
+++ sinkhorn.py
@@ -4,8 +4,8 @@ from firedrake import *
 from firedrake.pyplot import tripcolor
 from helpers import heat_kernel, gaussian_2d
 n = 30
-mean_0 = [0, 0]
-mean_1 = [1, 1]
+mean_0 = [0.1, 0.1]
+mean_1 = [0.5, 0.5]
 sigma = 0.1
 epsilon = 0.1
 
@@ -27,6 +27,7 @@ def sinkhorn(mu_0, mu_1, tol=1e-6, maxiter=1000, epsilon=0.1):
     new_v_1 = Function(V)
     res = 1
     v_0.assign(1.0)
+    v_1.assign(1.0) # so we don't get -inf errors
 
     u = TrialFunction(V)
     v = TestFunction(V)
@@ -43,20 +44,31 @@ def sinkhorn(mu_0, mu_1, tol=1e-6, maxiter=1000, epsilon=0.1):
     solv_1 = LinearVariationalSolver(prob_1, solver_parameters=params)
 
     n=0
-    while (tol < res) and (n < maxiter): 
+    while (tol < res) and (n < maxiter):
         solv_0.solve()
         solv_1.solve()
 
         res = (norm(new_v_0 - v_0) + norm(new_v_1 - v_1))
         print(res)
 
-        v_0.interpolate(new_v_0)
-        v_1.interpolate(new_v_1)
+        v_0.interpolate(mu_0 / new_v_1)
+        v_1.interpolate(mu_1 / new_v_0)
 
         n+=1
 
+    # is this -1 * epsilon?
     v_0.interpolate(epsilon * ln(v_0)) #phi
     v_1.interpolate(epsilon * ln(v_1)) #psi
     return v_0, v_1
 
 phi, psi = sinkhorn(mu_0, mu_1)
+
+plan = Function(V)
+plan.interpolate(phi * psi) # ignore ; this is a test
+x, y = SpatialCoordinate(mesh)
+
+fig, axes = plt.subplots()
+colors = tripcolor(plan, axes=axes)
+fig.colorbar(colors)
+plt.title("Sinkhorn Plan")
+plt.show()
