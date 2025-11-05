f223c2bc3ceed7155dbd85f662bdddcf4a2d598e
Kelan <k.gray25@imperial.ac.uk>
2025-11-05 09:45:37 +0000

    Add initialise environment function which handles mesh discretisation.

--- sinkhorn.py
+++ sinkhorn.py
@@ -1,25 +1,36 @@
+import matplotlib.pyplot as plt
+
 from firedrake import *
+from firedrake.pyplot import tripcolor, tricontour
 
 from solvers import HeatEquationSolver
 
 
+def initialise_env(epsilon, mesher, make_space):
+    """Initialise mesh and function space. Mesh discretisation fixed to be of order 1/(epsilon**2)"""
+    n = int(1 / (epsilon**2)) # Mesh size
+    mesh = mesher(30, 30)
+    v = make_space(mesh, "CG", 1)
+    return mesh, v
+
 # Constants
-N = 30
+EPSILON = 0.01
 MEAN_0 = [0.1, 0.1]
 MEAN_1 = [0.5, 0.5]
 SIGMA = 0.1
-EPSILON = 0.1
-MESH = UnitSquareMesh(N, N)
-V = FunctionSpace(MESH, "CG", 1)
 
-# Set up distributions
+# Initialise mesh and function space
+MESH, V = initialise_env(EPSILON, UnitSquareMesh, FunctionSpace)
+
+# Set up probability distributions
 mu_0 = Function(V)
 mu_1 = Function(V)
 
 x, y = SpatialCoordinate(MESH)
 mu_0.interpolate((1 / (2 * pi * SIGMA**2)) * exp(-((x- MEAN_0[0])**2 + (y - MEAN_0[1])**2) / (2 * SIGMA**2)))
 mu_1.interpolate((1 / (2 * pi * SIGMA**2)) * exp(-((x- MEAN_1[0])**2 + (y - MEAN_1[1])**2) / (2 * SIGMA**2)))
-# Normalise
+
+# Normalise on mesh
 Imu_0 = assemble(mu_0*dx)
 Imu_1 = assemble(mu_1*dx)
 mu_1.assign(mu_1/Imu_1)
@@ -53,9 +64,9 @@ def sinkhorn(
     Solver_1 = HeatEquationSolver(V, dt=epsilon/2)
     Solver_1.initialise()
 
-    n=0
+    i = 0
     res = 1
-    while (tol < res) and (n < maxiter):
+    while (tol < res) and (i < maxiter):
         Solver_1.solve()
         res = norm(
             Solver_0.function -
@@ -66,9 +77,8 @@ def sinkhorn(
         Solver_1.update(mu_1 / Solver_0.output_function)
 
         print(res)
-        n+=1
+        i += 1
 
-    # is this -1 * epsilon?
     phi.interpolate(epsilon * ln(Solver_0.function)) #phi
     psi.interpolate(epsilon * ln(Solver_1.function)) #psi
     return phi, psi
@@ -80,4 +90,7 @@ x, y = SpatialCoordinate(MESH)
 f = Function(Vc).interpolate(grad(phi))
 MESH.coordinates.assign(f)
 
-VTKFile("phi.pvd").write(mu_0)
+fig, axes = plt.subplots()
+colors = tripcolor(mu_0, axes=axes)
+fig.colorbar(colors)
+plt.show()
