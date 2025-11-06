2fd40097b9a8bab2e31974c41d83c97bdf436003
Advaith <advaithvelavan39@gmail.com>
2025-11-06 10:55:40 +0000

    fix some bugs in barycenter.py and visualise barycenters

--- barycenter.py
+++ barycenter.py
@@ -1,7 +1,9 @@
 from firedrake import *
 from sinkhorn import *
+import matplotlib.pyplot as plt
+from firedrake.pyplot import tripcolor
 
-def wasserstein_barycenter(mus, alphas, Vs, epsilons):
+def wasserstein_barycenter(mus, alphas):
     """
     Compute the Wasserstein barycenter of given distributions.
     """
@@ -14,13 +16,13 @@ def wasserstein_barycenter(mus, alphas, Vs, epsilons):
         raise e
     
     epsilon = 0.1
-    V = FunctionSpace(UnitSquareMesh(32, 32), "CG", 1)
+    V = FunctionSpace(UnitSquareMesh(10, 10), "CG", 1)
 
     mu = Function(V).assign(1.0)
 
-    v_list = [HeatEquationSolver(V, dt=epsilon/2)] * num_dists
-    w_list = [HeatEquationSolver(V, dt=epsilon/2)] * num_dists
-    d_list = [Function(V).assign(1.0)] * num_dists
+    v_list = [HeatEquationSolver(V, dt=epsilon/2) for _ in range(num_dists)]
+    w_list = [HeatEquationSolver(V, dt=epsilon/2) for _ in range(num_dists)]
+    d_list = [Function(V).assign(1.0) for _ in range(num_dists)]
 
     for i in range(num_dists):
         v_list[i].initialise()
@@ -29,19 +31,55 @@ def wasserstein_barycenter(mus, alphas, Vs, epsilons):
     # Placeholder for barycenter computation logic
 
     # THIS LOOP CAN BE PARALLELISED
-    for i in range(num_dists):
-        v_list[i].solve()
-        w_list[i].update(mu / v_list[i].output_function)
-        w_list[i].solve()
-        d_list[i].interpolate(v_list[i].function * w_list[i].output_function)
+    for j in range(10):
 
-        mu.interpolate(mu * (d_list[i] ** alphas[i]))
+        curr = [assemble(interpolate(mus[i], V)) for i in range(num_dists)]
 
-    
-    pass
+        for i in range(num_dists):
+            v_list[i].solve()
+            w_list[i].update(curr[i] / v_list[i].output_function)
+            w_list[i].solve()
+            d_list[i].interpolate(v_list[i].function * w_list[i].output_function)
+            mu.interpolate(mu * (d_list[i] ** alphas[i]))
 
+        for i in range(num_dists):
+            v_list[i].update(v_list[i].function * (mu / d_list[i]))
+        print("Barycenter computation complete.")
 
-    
+    return mu
 
+V = FunctionSpace(UnitSquareMesh(10, 10), "CG", 1)
 
+mean_0 = [0.1, 0.1]
+mean_1 = [0.9, 0.9]
+mean_2 = [0.3, 0.7]
 
+sigma_0 = 0.1
+sigma_1 = 0.1
+sigma_2 = 0.1
+
+mu_0 = Function(V)
+mu_1 = Function(V)
+mu_2 = Function(V)
+
+x, y = SpatialCoordinate(V.mesh())
+mu_0.interpolate((1 / (2 * pi * sigma_0**2)) * exp(-((x- mean_0[0])**2 + (y - mean_0[1])**2) / (2 * sigma_0**2)))
+mu_1.interpolate((1 / (2 * pi * sigma_1**2)) * exp(-((x- mean_1[0])**2 + (y - mean_1[1])**2) / (2 * sigma_1**2)))
+mu_2.interpolate((1 / (2 * pi * sigma_2**2)) * exp(-((x- mean_2[0])**2 + (y - mean_2[1])**2) / (2 * sigma_2**2)))
+
+# Normalise on mesh
+Imu_0 = assemble(mu_0*dx)
+Imu_1 = assemble(mu_1*dx)
+Imu_2 = assemble(mu_2*dx)
+mu_1.assign(mu_1/Imu_1)
+mu_0.assign(mu_0/Imu_0)
+mu_2.assign(mu_2/Imu_2)
+
+mus = [mu_0, mu_1]
+alphas = [0.45, 0.55]
+
+bary = wasserstein_barycenter(mus, alphas)
+fig, axes = plt.subplots()
+colors = tripcolor(bary, axes=axes)
+fig.colorbar(colors)
+plt.show()
