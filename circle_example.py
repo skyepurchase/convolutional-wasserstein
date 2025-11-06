3e75f61f1bd0a58dd41a6e86a923bcbfac8075b5
Kelan <k.gray25@imperial.ac.uk>
2025-11-06 11:32:13 +0000

    Initial commit for OT circle example

---
+++ circle_example.py
@@ -0,0 +1,38 @@
+from firedrake import *
+from firedrake.pyplot import tripcolor
+
+from scipy.special import iv # Bessel function 
+
+from sinkhorn import sinkhorn
+
+#Constants 
+EPSILON = 0.1
+N = 20
+R = 1
+KAPPA_0 = KAPPA_1 = 20
+MEAN_0 = 1
+MEAN_1 =1
+
+# Circle mesh
+MESH = CircleManifoldMesh(N, radius=R)
+V = FunctionSpace(MESH, "CG", 1)
+
+
+# Set up probability distributions
+mu_0 = Function(V)
+mu_1 = Function(V)
+
+x, y = SpatialCoordinate(MESH)
+theta = atan2(y, x)
+
+#Von Mises density
+mu_0.interpolate(exp(KAPPA_0 * cos(theta - MEAN_0)) / (2.0 * pi * iv(0, KAPPA_0)))
+mu_1.interpolate(exp(KAPPA_1 * cos(theta - MEAN_1)) / (2.0 * pi * iv(0, KAPPA_1)))
+
+# Normalise on mesh
+Imu_0 = assemble(mu_0*dx)
+Imu_1 = assemble(mu_1*dx)
+mu_1.assign(mu_1/Imu_1)
+mu_0.assign(mu_0/Imu_0)
+
+phi, psi = sinkhorn(mu_0, mu_1, V, epsilons=[EPSILON])
